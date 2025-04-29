37a6ae8a819d03227451c1d0e9d96f61a8aaeca6
Manasi Mhatre <mr75mhatre2006@gmail.com>
2025-04-29 12:21:50 +0530

    phase 2 done

--- phase1.cpp
+++ phase1.cpp
@@ -1,241 +1,270 @@
-#include <iostream>
+#include <algorithm>
 #include <fstream>
-#include <vector>
-#include <cstring>
-#include <cstdlib>
-
+#include <iostream>
+#include <string>
 using namespace std;
 
-char M[100][4];
-char buffer[40];
-char IR[4];
-char R[4];
-bool C = false;
-int IC = 0;
-int SI = 0;
+class VM
+{
+private:
+    char buffer[40];
+    char Memory[100][4];
+    char IR[4];  
+    char R[4];   
+    bool C;     
+    int IC;     
+    int SI;     
+    fstream infile;
+    ofstream outfile;
 
-void loadProgram(const string& filename);
-void startExecution();
-void executeUserProgram();
-void masterMode(string operand);
-void readData(string operand);
-void writeData(string operand);
-void customTerminate();
-void printMemory();
-
-int main() {
-    for (int i = 0; i < 100; i++) {
-        for (int j = 0; j < 4; j++) {
-            M[i][j] = '\0';
-        }
-    }
-    cout << "Memory Initialized." << endl;
-    loadProgram("input.txt");
-    cout << "Program Loaded." << endl;
-    printMemory();
-    startExecution();
-    printMemory();
-    cout << "Execution Complete." << endl;
-    return 0;
-}
-
-void printMemory() {
-    cout << "\nMain Memory:\n";
-    cout << "-------------------------------------------------\n";
-    for (int i = 0; i < 100; i++) {
-        cout << "M[" << i << "] = ";
-        for (int j = 0; j < 4; j++) {
-            cout << (M[i][j] ? M[i][j] : '.');  
-        }
-        cout <<  "\n";
-    }
-    cout << "-------------------------------------------------\n";
-}
-
-void printSystemStatus() {
-    cout << "\nSystem Status:\n";
-    cout << "------------------------\n";
-    cout << "IC: " << IC << endl;
-    cout << "IR: " << IR[0] << IR[1] << IR[2] << IR[3] << endl;
-    cout << "R : " << R[0] << R[1] << R[2] << R[3] << endl;
-    cout << "C : " << (C ? "True" : "False") << endl;
-    cout << "------------------------\n";
-}
-
-
-void loadProgram(const string& filename) {
-    ifstream fin(filename);
-    string line;
-    int m = 0;
-
-    if (!fin) {
-        cerr << "Error opening file." << endl;
-        exit(1);
+    void init(){
+        fill(buffer, buffer + sizeof(buffer), '\0');
+        fill(&Memory[0][0], &Memory[0][0] + sizeof(Memory), '\0');
+        fill(IR, IR + sizeof(IR), '\0');
+        fill(R, R + sizeof(R), '\0');
+        IC = 0;
+        C = false;
+        SI = 0;
     }
 
-    while (getline(fin, line)) {
-        if (line.substr(0, 4) == "$AMJ") continue;
-        if (line.substr(0, 4) == "$DTA") break;
-        if (line.substr(0, 4) == "$END") break;
-        int len = line.length();
-
-       strncpy(buffer, line.c_str(), len);
-
-        if (m >= 100) {
-            cout << "Memory overflow. Aborting..." << endl;
-            exit(0);
-        }
-
-        for (int i = 0; i < len; i += 4, m++) { 
-            if (m >= 100) break;
-            memset(M[m], 0, 4); 
-            strncpy(M[m], buffer + i, 4);
-        }
-        
+    void resetBuffer(){
+        fill(buffer, buffer + sizeof(buffer), '\0');
     }
-    fin.close();
-}
 
-void startExecution() {
-    IC = 0;
-    executeUserProgram();
-}
-
-void executeUserProgram() {
-    while (true) {
-        strncpy(IR, M[IC], 4);
-        IC++;
-
-        string opcode(IR, 2);
-        string operand = (strlen(IR) >= 3) ? string(IR + 2, 2) : "";
-
-        cout << "\nExecuting Instruction: " << opcode << " " << operand << endl;
-        printSystemStatus();
-
-        cout << "Opcode: " << opcode << ", Operand: " << operand << endl;
-        
-        int address = 0;
-        if (!operand.empty() && opcode != "H") {
-            for (char c : operand) {
-                if (c >= '0' && c <= '9') {
-                    address = address * 10 + (c - '0');
-                } else {
-                    cout << "Bad Input" << endl;
-                    return; 
-                }
-            }
-        }
-
-        if (opcode == "LR") {
-            strncpy(buffer, M[address], 4);
-            strncpy(R, buffer, 4);
-        } else if (opcode == "SR") {
-            strncpy(buffer, R, 4);
-            strncpy(M[address], buffer, 4);
-        } else if (opcode == "CR") {
-            C = (strncmp(R, M[address], 4)  == 0);
-        } else if (opcode == "BT") {
-            if (C) IC = address;
-        } else if (opcode == "GD") {
-            SI = 1;
-            masterMode(operand);
-        } else if (opcode == "PD") {
-            SI = 2;
-            masterMode(operand);
-        } else if (opcode == "H") {
-            SI = 3;
-            masterMode("");
-            return;
-        } else {
-            return;
-        }
-    }
-}
-
-void masterMode(string operand) {
-    switch (SI) {
+    void MOS()
+    { 
+        switch (SI)
+        {
         case 1:
-            readData(operand);
+            READ();
             break;
+
         case 2:
-            writeData(operand);
+            WRITE();
             break;
+
         case 3:
-            customTerminate();
-            break;
-    }
-}
-
-void readData(string operand) {
-    ifstream fin("input.txt");
-    string line;
-    int start = stoi(operand);
-
-    if (!fin) {
-        cerr << "Error opening input file." << endl;
-        exit(1);
-    }
-
-    while (getline(fin, line)) {
-        if (line == "$DTA") {  
-            int i = 0;
-            while (getline(fin, line)) {
-                if (line.find("$END") != string::npos) break; 
-
-                int len = line.length();
-                int chunks = (len + 3) / 4; 
-
-                memset(buffer, 0, sizeof(buffer));
-                strncpy(buffer, line.c_str(), sizeof(buffer) - 1); 
-
-                for (int j = 0; j < chunks; j++) {
-                    if (start + i >= 100) {
-                        cerr << "Memory overflow while reading data." << endl;
-                        exit(1);
-                    }
-                    memset(M[start + i], 0, 4);  
-                    strncpy(M[start + i], buffer + j * 4, 4);
-                    i++;
-                }
-            }
+            TERMINATE();
             break;
         }
-    }
-    fin.close();
-    SI = 0;
-}
-
-void writeData(string operand) {
-    ofstream fout("output.txt", ios::app);
-    int start = stoi(operand);
-
-    if (!fout) {
-        cerr << "Error opening output file." << endl;
-        exit(1);
+        SI = 0;
     }
 
-    string output;
-    memset(buffer, 0, sizeof(buffer)); 
-    for (int i = 0; i < 10 && (start + i) < 100; i++) {
-        if (M[start + i][0] == '\0') break;  
-        strncpy(buffer + (i * 4), M[start + i], 4);
+
+    void LOAD(){
+
+        if (infile.is_open())
+        {
+            string s;
+            while (getline(infile, s))
+            { 
+                if (s[0] == '$' && s[1] == 'A' && s[2] == 'M' && s[3] == 'J'){
+                    init();
+                    cout<<"New Job started\n";
+                } else if (s[0] == '$' && s[1] == 'D' && s[2] == 'T' && s[3] == 'A'){
+                    cout << "Data card loding\n";
+                    resetBuffer();
+                    STARTEXE();
+                    printMemory();
+                } else if (s[0] == '$' && s[1] == 'E' && s[2] == 'N' && s[3] == 'D'){
+                    cout << "END of Job\n";
+                } else{
+                    cout << "Program Card loding\n";
+                    int length = s.size();
+                    resetBuffer();  
+
+                    for (int i = 0; i < length; i++){
+                        buffer[i] = s[i];
+                    }
+
+                    int buff = 0;
+                    int ref = 0;
+
+                    while (buff < 40 && buffer[buff] != '\0'){
+                        for (int j = 0; j < 4; j++){
+                            if (buffer[buff] == 'H'){
+                                Memory[IC][j] = 'H';
+                                buff++;
+                                break;
+                            }
+                            Memory[IC][j] = buffer[buff];
+                            buff++;
+                        }
+                        IC++;
+                    }
+                }
+
+                for(int i = 0; i<100; i++)
+                { 
+                    cout<<"M["<<i<<"]\t";
+                    for(int j = 0; j<4; j++ )
+                    {
+                       
+                        cout<<Memory[i][j];
+                        
+                    }
+                    cout<<endl;
+                }
+                
+            }
+            infile.close();
+        }
     }
 
-    output.append(buffer); 
-    fout << output << endl; 
-    fout.close();
-    SI = 0;
-}
-
-
-void customTerminate() {
-    ofstream fout("output.txt", ios::app);
-    if (!fout) {
-        cerr << "Error opening output file." << endl;
-        exit(1);
+    void STARTEXE(){
+        IC = 0;
+        EXECUTEUSERPROGRAM();
     }
-    fout << endl;
-    fout.close();
-    cout << "Program Terminated." << endl;
-    SI = 0;
+
+    void READ()
+    {
+        cout << "Read function called\n";
+
+        string data;
+        getline(infile, data);
+        int len = data.size();
+        for (int i = 0; i < len; i++)
+        {
+            buffer[i] = data[i];
+        }
+        int buff = 0, mem_ptr = (IR[2] - '0') * 10;
+        while (buff < 40 && buffer[buff] != '\0')
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Memory[mem_ptr][i] = buffer[buff];
+                buff++;
+            }
+            mem_ptr++;
+        }
+        resetBuffer();
+    }
+
+    void WRITE()
+    {
+        cout << "Write function called\n";
+        outfile.open("output.txt", ios::app);
+
+        for (int i = (IR[2] - '0') * 10; i < (IR[2] - '0' + 1) * 10; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                // if (Memory[i][j] != '\0')
+                // {
+                //     outfile << Memory[i][j];
+                // }
+                outfile << Memory[i][j];
+            }
+        }
+        outfile << "\n";
+        outfile.close();
+    }
+
+    void TERMINATE()
+    {
+        outfile.open("output.txt", ios::app);
+        cout << "Terminate called\n\n";
+        outfile << "\n\n";
+        outfile.close();
+    }
+
+    void EXECUTEUSERPROGRAM(){ 
+        while (IC <= 99 && Memory[IC][0] != '\0'){
+
+            for (int i = 0; i < 4; i++){
+                IR[i] = Memory[IC][i];
+            }
+            IC++;
+
+            printSystemStatus();
+
+            if (IR[0] == 'G' && IR[1] == 'D'){
+                SI = 1;
+                MOS();
+            } else if (IR[0] == 'P' && IR[1] == 'D'){
+                SI = 2;
+                MOS();
+            } else if (IR[0] == 'H'){
+                SI = 3;
+                MOS();
+                return;
+            } else if (IR[0] == 'L' && IR[1] == 'R'){
+                int address = (IR[2] - '0') * 10 + (IR[3] - '0');
+    
+                for (int i = 0; i < 4; i++) {
+                    buffer[i] = Memory[address][i];
+                }
+    
+                for (int i = 0; i < 4; i++) {
+                    R[i] = buffer[i];
+                }
+                cout << "LR: Loaded from M[" << address << "] to R via buffer" << endl;
+            } else if (IR[0] == 'S' && IR[1] == 'R'){
+                int address = (IR[2] - '0') * 10 + (IR[3] - '0');
+    
+                for (int i = 0; i < 4; i++) {
+                    buffer[i] = R[i];
+                }
+        
+                for (int i = 0; i < 4; i++) {
+                    Memory[address][i] = buffer[i];
+                }
+                cout << "SR: Stored from R to M[" << address << "] via buffer" << endl;
+            } else if (IR[0] == 'C' && IR[1] == 'R'){
+                int cnt = 0;
+                for (int i = 0; i < 4; i++)
+                    if (Memory[(IR[2] - '0') * 10 + (IR[3] - '0')][i] == R[i])
+                        cnt++;
+               
+                if (cnt == 4)
+                    C = true;
+                else
+                    C = false;
+            } else if (IR[0] == 'B' && IR[1] == 'T'){
+                if (C){
+                    IC = (IR[2] - '0') * 10 + (IR[3] - '0');
+                }
+            }
+        }
+    }
+
+    void printMemory() {
+        cout << "\nMain Memory:\n";
+        cout << "-------------------------------------------------\n";
+        for (int i = 0; i < 100; i++) {
+            cout << "M[" << i << "] = ";
+            for (int j = 0; j < 4; j++) {
+                cout << (Memory[i][j] ? Memory[i][j] : '.');  
+            }
+            cout <<  "\n";
+        }
+        cout << "-------------------------------------------------\n";
+    }
+
+    void printSystemStatus() {
+        cout << "\nSystem Status:\n";
+        cout << "------------------------\n";
+        cout << "IC: " << IC << endl;
+        cout << "IR: " << IR[0] << IR[1] << IR[2] << IR[3] << endl;
+        cout << "R : " << R[0] << R[1] << R[2] << R[3] << endl;
+        cout << "C : " << (C ? "True" : "False") << endl;
+        cout << "------------------------\n";
+    }
+
+public:
+    VM()
+    {
+        infile.open("./input.txt", ios::in);
+        init();
+        cout << "Memory Initialized." << endl;
+        LOAD();
+        printMemory();
+        cout << "Execution Complete." << endl;
+    }
+};
+
+int main()
+{
+    VM v;
+    return 0;
 }
